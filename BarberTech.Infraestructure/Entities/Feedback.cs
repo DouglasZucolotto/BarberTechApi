b06aa0342c1b1198702e2d14f11e55643fec9fb6
Douglas Zucolotto Gonçalves <douglaszuco2@gmail.com>
2023-12-29 15:46:00 -0300

    infra feito

---
+++ BarberTech.Infraestructure/Entities/Feedback.cs
@@ -0,0 +1,24 @@
+﻿namespace BarberTech.Infraestructure.Entities
+{
+    public class Feedback
+    {
+        public Guid Id { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public string? Comment { get; set; }
+
+        public int Qnt_stars { get; set; }
+
+        public Guid HaircutId { get; set; }
+
+        public Feedback(Guid id, Guid userId, string? comment, int qnt_stars, Guid haircutId)
+        {
+            Id = id;
+            UserId = userId;
+            Comment = comment;
+            Qnt_stars = qnt_stars;
+            HaircutId = haircutId;
+        }
+    }
+}
